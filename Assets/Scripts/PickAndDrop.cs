a80764f3ac13ce08e1ae6cf051081f946024fdae
0shiros <oli2014@gmx.fr>
2025-09-24 16:33:51 +0200

    Pick and drop atom, and atom collids even if pick

---
+++ Assets/Scripts/PickAndDrop.cs
@@ -0,0 +1,62 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickAndDrop : MonoBehaviour
+{
+    [SerializeField] private float lerpTime = 0.1f;
+    private Vector2 mousePosition;
+    private Vector2 atomPosition;
+    private Collider2D atomCollider;
+    private Rigidbody2D atomRigidbody;
+    private bool isPicking = false;
+
+    private void Start()
+    {
+        atomPosition = transform.position;
+        atomRigidbody = transform.GetComponent<Rigidbody2D>();
+        atomCollider = transform.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        AtomDetection();
+    }
+
+    private void FixedUpdate()
+    {
+        PickOrDrop();
+    }
+
+    private void AtomDetection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            
+            if (hit.collider != null && hit.collider == atomCollider)
+            {
+                isPicking = true;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+             isPicking = false;
+        }
+    }
+
+    private void PickOrDrop()
+    {
+        if (isPicking) 
+        {
+             atomRigidbody.linearVelocity = Vector2.zero;
+             atomRigidbody.MovePosition(Vector2.Lerp(atomPosition, mousePosition, lerpTime));
+        }
+        else
+        {
+            isPicking = false;
+        }
+    }
+}
