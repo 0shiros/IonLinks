e684dc1f08e0224d283b901e71b3d923c433e711
0shiros <oli2014@gmx.fr>
2025-10-06 17:06:23 +0200

    Fix error last level

--- Assets/Scripts/Battery_Ampoule/Shine.cs
+++ Assets/Scripts/Battery_Ampoule/Shine.cs
@@ -25,8 +25,11 @@ public class Shine : MonoBehaviour
       if(!victoryPanel.activeSelf) victoryPanel.SetActive(true);
      
       //Unlock the next level in selector level
-      PlayerPrefs.SetInt("chains"+nextLevel, 1);
-      PlayerPrefs.Save();
+      if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount-1)
+      {
+         PlayerPrefs.SetInt("chains" + nextLevel, 1);
+         PlayerPrefs.Save();
+      }
    }
    
 }
